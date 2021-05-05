ef003e68a8da7da98dd2f77278d142fa5cc39234
sunny-karthik <bhupathirajusairamkarthikvarma.181me116@nitk.edu.in>
2021-05-05 13:18:42 +0530

    v10.01

--- input.m
+++ input.m
@@ -2,9 +2,9 @@
 c1=1.2801;   % maximum value of friction curve
 c2=23.99;    % friction curve shape
 c3=0.52;     % friction curve difference between the maximum value and the value at lambda =1
-c4=0.33;     % wetness characteristic value (Lies b/w 0.02 and 0.04 s/m)
+c4=0.03;     % wetness characteristic value (Lies b/w 0.02 and 0.04 s/m)
 m=342; %kg
 r=0.33; %m
 jw=1.13; %kg-m2
 fn=m*9.81;
-tb=200;
+
